5c3ad42dc050dde11cfdcde24eb79913cf780384
Timo Stollenwerk <stollenwerk@kitconcept.com>
2021-11-21 22:29:39 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -22,9 +22,12 @@ setup(
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Environment :: Web Environment",
+        "Development Status :: 1 - Planning",
         "Framework :: Plone",
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
