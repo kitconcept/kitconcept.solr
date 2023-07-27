e6d08ff25229727c12e1061365f8640d4a84e9b7
Érico Andrei <ericof@gmail.com>
2023-07-27 11:48:07 -0300

    Use a higher timeout to allow the CI to build the Solr image

--- tests/services/conftest.py
+++ tests/services/conftest.py
@@ -7,8 +7,8 @@ from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
 from plone.namedfile import NamedBlobImage
 from plone.restapi.testing import RelativeSession
+from requests import exceptions as exc
 from typing import List
-from urllib3 import exceptions as exc
 from zope.component.hooks import setSite
 
 import pytest
@@ -106,7 +106,7 @@ def is_responsive(url):
         response = requests.get(url)
         if response.status_code == 200:
             return b"""<str name="status">OK</str>""" in response.content
-    except (ConnectionError, exc.ProtocolError, exc.TimeoutError):
+    except (exc.ConnectionError, exc.Timeout):
         return False
 
 
@@ -122,7 +122,7 @@ def solr_service(docker_ip, docker_services):
     port = docker_services.port_for("solr", 8983)
     url = f"http://{docker_ip}:{port}/solr/plone/admin/ping?wt=xml"
     docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
+        timeout=90.0, pause=0.1, check=lambda: is_responsive(url)
     )
     return url
 
